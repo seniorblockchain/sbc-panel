23eeb87a9f0a7d223e180983677cef83d7604b44
Milad Raeisi <miladsoft@yahoo.com>
2025-07-19 08:22:36 +0400

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,66 @@
-import { Button } from "@/components/ui/button"
+// NOTE: This file should normally not be modified unless you are adding a new provider.
+// To add new routes, edit the AppRouter.tsx file.
 
-function App() {
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createHead, UnheadProvider } from '@unhead/react/client';
+import { InferSeoMetaPlugin } from '@unhead/addons';
+import { Suspense } from 'react';
+import NostrProvider from '@/components/NostrProvider';
+import { Toaster as Sonner, Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { NostrLoginProvider } from '@nostrify/react/login';
+import { AppProvider } from '@/components/AppProvider';
+import AppRouter from './AppRouter';
+import type { AppConfig } from "./contexts/AppContext";
+
+const head = createHead({
+  plugins: [
+    InferSeoMetaPlugin(),
+  ],
+});
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 60000, // 1 minute
+      gcTime: Infinity,
+    },
+  },
+});
+
+const defaultConfig: AppConfig = {
+  theme: "light",
+  relayUrl: "wss://relay.nostr.band",
+};
+
+const presetRelays = [
+  { url: 'wss://ditto.pub/relay', name: 'Ditto' },
+  { url: 'wss://relay.nostr.band', name: 'Nostr.Band' },
+  { url: 'wss://relay.damus.io', name: 'Damus' },
+  { url: 'wss://relay.primal.net', name: 'Primal' },
+];
+
+export function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  )
+    <UnheadProvider head={head}>
+      <AppProvider storageKey="nostr:app-config" defaultConfig={defaultConfig} presetRelays={presetRelays}>
+        <QueryClientProvider client={queryClient}>
+          <NostrLoginProvider storageKey='nostr:login'>
+            <NostrProvider>
+              <TooltipProvider>
+                <Toaster />
+                <Sonner />
+                <Suspense>
+                  <AppRouter />
+                </Suspense>
+              </TooltipProvider>
+            </NostrProvider>
+          </NostrLoginProvider>
+        </QueryClientProvider>
+      </AppProvider>
+    </UnheadProvider>
+  );
 }
 
-export default App
+export default App;
